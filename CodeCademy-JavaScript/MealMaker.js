dd58171977bde4af77fc4f5003d9375da5ae3807
KristianWB <42993999+KristianWB@users.noreply.github.com>
2019-09-27 10:34:37 +0200

    task 2-6

--- CodeCademy-JavaScript/MealMaker.js
+++ CodeCademy-JavaScript/MealMaker.js
@@ -1,1 +1,25 @@
-let menu = {}
+let menu = {
+    _courses: {
+      appetizers: [],
+      mains: [],
+      desserts: [],
+    },
+    
+      get appetizers() {},  
+      set appetizers(appetizerIn) {},
+    
+    get mains() {},
+    set mains(mainsIn) {},
+    
+    get desserts() {},
+    set desserts(dessertsIn) {},
+    
+    get courses() {
+      return {
+        appetizers: this.appetizers,
+        mains: this.mains,
+        desserts: this.desserts,
+      } ;
+    },
+    
+  };

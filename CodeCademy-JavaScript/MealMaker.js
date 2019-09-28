12123e59e0c034882659460bc2012aa8afe281e7
KristianWB <42993999+KristianWB@users.noreply.github.com>
2019-09-28 09:25:57 +0200

    added function

--- CodeCademy-JavaScript/MealMaker.js
+++ CodeCademy-JavaScript/MealMaker.js
@@ -1,33 +1,39 @@
 const menu = {
-    _courses: {
-      appetizers: [],
-      mains: [],
-      desserts: [],
-    },
-    
-      get appetizers() {},  
-      set appetizers(appetizerIn) {},
-    
-    get mains() {},
-    set mains(mainsIn) {},
-    
-    get desserts() {},
-    set desserts(dessertsIn) {},
-    
-    get courses() {
-      return {
-        appetizers: this.appetizers,
-        mains: this.mains,
-        desserts: this.desserts,
-      } ;
-    },
-    
-    addDishToCourse(courseName, dishName, dishPrice) {
-     const dish = {
-       dishName: dishName,
-       dishPrice: dishPrice
-     } 
-     this._courses[courseName].push(dish);
+  _courses: {
+    appetizers: [],
+    mains: [],
+    desserts: [],
+  },
   
-    }  
-  };
+	get appetizers() {},  
+	set appetizers(appetizerIn) {},
+  
+  get mains() {},
+  set mains(mainsIn) {},
+  
+  get desserts() {},
+  set desserts(dessertsIn) {},
+  
+  get courses() {
+    return {
+      appetizers: this.appetizers,
+      mains: this.mains,
+      desserts: this.desserts,
+    } ;
+  },
+  
+  addDishToCourse(courseName, dishName, dishPrice) {
+   const dish = {
+     dishName: dishName,
+     dishPrice: dishPrice
+   } 
+   this._courses[courseName].push(dish);
+
+  },
+  
+  getRandomDishFromCourse(courseName) {
+    let dishes = this.courses[courseName];
+    const randomIndex = Math.floor(Math.random()*dishes.length);
+    return dishes[randomIndex];
+  }
+};

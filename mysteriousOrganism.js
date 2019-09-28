ed8befb4c7e63d1bf31954326f412db8ff8c5d17
KristianWB <42993999+KristianWB@users.noreply.github.com>
2019-09-28 18:32:41 +0200

    New fileProject - mysteriousOrganism

---
+++ mysteriousOrganism.js
@@ -0,0 +1,30 @@
+// Returns a random DNA base
+const returnRandBase = () => {
+    const dnaBases = ['A', 'T', 'C', 'G']
+    return dnaBases[Math.floor(Math.random() * 4)] 
+  }
+  
+  // Returns a random single stand of DNA containing 15 bases
+  const mockUpStrand = () => {
+    const newStrand = []
+    for (let i = 0; i < 15; i++) {
+      newStrand.push(returnRandBase())
+    }
+    return newStrand
+  }
+  
+  function pAequorFactory(number , dnaSequence) {
+    return {
+      specimenNum: number,
+      dna: [dnaSequence],
+      mutate(){
+        dna[Math.floor(Math.random() * dna.length)];
+        returnRandBase()
+      }
+    }
+  };
+  
+  console.log(pAequorFactory(1, mockUpStrand()));
+  
+  
+  

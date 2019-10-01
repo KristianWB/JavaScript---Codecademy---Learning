3fbd4fa097b2500e51b9743764f8ed41d092df47
KristianWB <42993999+KristianWB@users.noreply.github.com>
2019-10-01 15:09:28 +0200

    lots o stuff

--- booksNstuff.js
+++ booksNstuff.js
@@ -1,1 +1,52 @@
-// Beginning
+class Media{
+    constructor(title){
+      this._title = title;
+      this._isCheckedOut = false;
+      this._ratings = [];
+    }
+    
+    get title(){
+      return this._title;
+    }
+    get isCheckedOut(){
+      return this._isCheckedOut;
+    }
+    get ratings(){
+      return this._ratings;
+    }
+    
+    set isCheckedOut(boolean){
+          this._isCheckedOut = boolean;
+    }
+    
+    toggleCheckOutStatus() {
+      if(this._isCheckedOut === false){
+        this._isCheckedOut = true;
+      } else {
+            this._isCheckedOut = false;
+      }
+    }
+    
+      //6.7
+    
+    getAverageRating(){
+      let sumOfRating = this.ratings.reduce(
+    (accumulator, currentValue) => accumulator + currentValue, 0);
+      let averageRating = sumOfRating/this.ratings.length
+      return averageRating
+    }
+    
+    //6.8
+    addRating(number){
+      this._ratings.push(number)
+    }
+    
+  }
+  
+  let test = new Media('booHoo')
+  test.addRating(1)
+  test.addRating(2)
+  test.addRating(3)
+  test.addRating(4)
+  test.addRating(5)
+  console.log(test.getAverageRating())

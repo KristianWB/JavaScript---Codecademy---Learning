532b9ae0ddd5517d1447952b1e551774637e1b45
KristianWB <42993999+KristianWB@users.noreply.github.com>
2019-10-01 15:29:45 +0200

    8.16 - testing method

--- booksNstuff.js
+++ booksNstuff.js
@@ -83,6 +83,15 @@ class Media{
   //6.15
   let historyOfEverything = new Book('Bill Bryson', 'A Short History of Nearly Everything', 544)
   
+  console.log('8.15')
   console.log(historyOfEverything)
+  
+  //8.16
+  historyOfEverything.toggleCheckOutStatus()
+  console.log('8.16')
+    console.log(historyOfEverything)
+  
+  //8.17
+  
   
   

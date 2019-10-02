bc59a886d29370bfb90fe4f10d8ff0df4f0388ac
KristianWB <42993999+KristianWB@users.noreply.github.com>
2019-10-02 11:01:16 +0200

    retry: 8.15 - method test

--- schoolCatalogue.js
+++ schoolCatalogue.js
@@ -79,5 +79,6 @@ class School {
   let lorraineHansbury = new PrimarySchool('Lorraine Hansbury', 514, 'Students must be picked up by a parent, guardian, or a family member over the age of 13.')
   console.log(lorraineHansbury)
   
+  //8.15
   lorraineHansbury.quickFacts()
   

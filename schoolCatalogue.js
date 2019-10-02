6104c8ec2d2067dee82fd112aa78147cd728c200
KristianWB <42993999+KristianWB@users.noreply.github.com>
2019-10-02 08:43:56 +0200

    1.3 - properties

--- schoolCatalogue.js
+++ schoolCatalogue.js
@@ -2,8 +2,10 @@
 
 //1.1
 class School {
-    //1.2 - Building Constructor with parameters
+    //1.2 - Building Constructor
     constructor(name, level, numberOfStudents){
+      
+      //1.3 - Setting up properties
       this._name=name;
       this._level=level;
       this._numberOfStudents=numberOfStudents;

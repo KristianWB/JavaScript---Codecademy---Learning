4fc646705ec23447441e3021a9454d4f8bcde288
KristianWB <42993999+KristianWB@users.noreply.github.com>
2019-10-08 10:34:25 +0200

    #5 - export default

---
+++ CodeCademy-JavaScript/Modules/airplane.js
@@ -0,0 +1,21 @@
+//#5 - export default
+
+//#5.1 - create module
+let Airplane = {
+    //#5.2 - creating variables
+        availableAirplanes: [
+        //#5.3 - Creating airplane objects
+        {
+          name: 'AeroJet',
+         fuelCapacity: 800
+        },
+        {
+          name: 'SkyJet',
+          fuelCapacity: 500
+        }
+      ],
+    };
+    
+    
+    
+    export default Airplane;

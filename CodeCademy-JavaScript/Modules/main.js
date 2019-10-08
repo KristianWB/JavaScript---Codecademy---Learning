91d4ee1a2d996dad3b68367ba5682f4f3007c835
KristianWB <42993999+KristianWB@users.noreply.github.com>
2019-10-08 09:02:39 +0200

    new subject - Modules

---
+++ CodeCademy-JavaScript/Modules/main.js
@@ -0,0 +1,8 @@
+//#2.1 - Create object Airplane
+let Airplane = {};
+
+//#2.2 - add property to Airplane
+Airplane.myAirplane = "StarJet";
+
+//#2.3 - export module Airplane
+module.exports = Airplane;
